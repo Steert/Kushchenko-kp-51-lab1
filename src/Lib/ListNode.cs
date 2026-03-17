ad304320273be9967b3b8325be443687f89416cb
Danil Kuschenko <lagasifo@gmail.com>
2026-03-17 19:57:19 +0200

    Add: entities

---
+++ src/Lib/ListNode.cs
@@ -0,0 +1,13 @@
+﻿namespace Lib;
+
+public class ListNode
+{
+    public Record val;
+    public ListNode next;
+
+    public ListNode(Record val = null, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/Lib/Record.cs
@@ -0,0 +1,24 @@
+﻿namespace Lib;
+
+public class Record
+{
+    public string athleteId { get; set; }
+    public string fullName { get; set; }
+    public string team { get; set; }
+    public int resultSeconds { get; set; }
+    
+    public Record(){}
+
+    public Record(string id, string fullName, string team, int resultSeconds)
+    {
+        athleteId = id;
+        this.fullName = fullName;
+        this.team = team;
+        this.resultSeconds = resultSeconds;
+    }
+
+    public override string ToString()
+    {
+        return $"{athleteId} - {fullName} - {team} -  {resultSeconds}";
+    }
+}
