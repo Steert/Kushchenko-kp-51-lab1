381a7f1d452de22a3aa36ea892710045890e4b99
Danil Kuschenko <lagasifo@gmail.com>
2026-03-17 19:57:38 +0200

    Add: sort logic

---
+++ src/Lib/SortStatistics.cs
@@ -0,0 +1,12 @@
+﻿namespace Lib;
+
+public class SortStatistics
+{
+    public int AmountOfComparison { get; set; }
+    public int AmountOfIterations { get; set; }
+
+    public override string ToString()
+    {
+        return $"Comparison: {AmountOfComparison}, Iterations: {AmountOfIterations}";
+    }
+}
---
+++ src/Lib/Sorter.cs
@@ -0,0 +1,86 @@
+﻿namespace Lib;
+
+public class Sorter
+{
+    
+    public static Record[] BucketSort(Record[] array, SortStatistics stats)
+    {
+        int bsize = SearchMax(array, stats) / 5 + 1;
+
+        ListNode[] buckets = new ListNode[bsize];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            int index = array[i].resultSeconds / 5;
+            
+            InsertInCorrectPos(ref buckets[index], array[i], stats);
+        }
+        
+        CopyToArray(array, buckets, stats);
+
+        return array;
+    }
+
+    public static int SearchMax(Record[] array, SortStatistics stats)
+    {
+        int max = array[0].resultSeconds;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            stats.AmountOfComparison++;
+            stats.AmountOfIterations++;
+            if (array[i].resultSeconds > max)
+            {
+                max = array[i].resultSeconds;
+            }
+        }
+
+        return max;
+    }
+
+    public static ListNode InsertInCorrectPos(ref ListNode listNode, Record value, SortStatistics stats)
+    {
+        stats.AmountOfComparison++;
+        if (listNode == null || listNode.val.resultSeconds > value.resultSeconds)
+        {
+            listNode = new ListNode(value, listNode);
+            return listNode;
+        }
+        
+        ListNode current = listNode;
+        while (current.next != null)
+        {
+            stats.AmountOfComparison++;
+            stats.AmountOfIterations++;
+            if (current.next.val.resultSeconds > value.resultSeconds)
+            {
+                break;
+            }
+            current = current.next;
+        }
+        
+        ListNode result = new ListNode(value);
+        result.next = current.next;
+        current.next = result;
+
+        return result;
+    }
+
+    public static Record[] CopyToArray(Record[] array, ListNode[] buckets, SortStatistics stats)
+    {
+        int counter = 0;
+        for (int i = 0; i < buckets.Length; i++)
+        {
+            ListNode current = buckets[i];
+
+            while (current != null)
+            {
+                stats.AmountOfIterations++;
+                array[counter++] = current.val;
+                current = current.next;
+            }
+        }
+
+        return array;
+    }
+}
